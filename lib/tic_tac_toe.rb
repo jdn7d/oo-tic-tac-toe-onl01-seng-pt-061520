1124ba30debe6f7bc6245fc7c39b92e4c1a7b13b
Jessie Donovan <donovanajessie@gmail.com>
2020-07-13 00:30:26 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,28 @@
+class TicTacToe
+
+  def initialize
+    @board = []
+  end
+
+  def WIN_COMBINATIONS
+
+  end
+
+  def display_board
+    puts @board
+  end
+
+  def move(i,token = "X")
+
+  end
+
+  def input_to_index(inpute)
+
+  end
+
+  def position_taken?
+
+  end
+
+  def
+end
