f52a955cc224878e4c1af08b511bc3c8d5030952
Jessie Donovan <donovanajessie@gmail.com>
2020-07-13 02:27:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,4 +52,18 @@ end
  turn_count.even? ? "X" : "O"
   end
 
+  def turn
+       puts "Input a number (1-9):"
+       user_input = gets.strip
+       index = input_to_index(user_input)
+       if valid_move?(index)
+         token = current_player
+         move(index, token)
+       else
+         turn
+       end
+       display_board
+     end
+  end
+
 end
