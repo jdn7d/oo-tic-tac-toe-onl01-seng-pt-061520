5f1d095350d719d7d5680fad32e6d62b9ad9fbeb
Jessie Donovan <donovanajessie@gmail.com>
2020-07-13 01:37:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,10 +8,11 @@ class TicTacToe
 
 
   def display_board
-    puts "#{@board[0]} | #{@board[1]} | #{@board[2]} \n ----------- \n",
-    "#{@board[3]} | #{@board[4]} | #{@board[5]} \n",
-    "----------- \n",
-    "#{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts " -----------"
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}",
+    puts " -----------"
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
 
   def move(i,token = "X")
