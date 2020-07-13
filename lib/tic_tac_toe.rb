ad7129b71a1e9511bec11edf3507ef885b2efeec
Jessie Donovan <donovanajessie@gmail.com>
2020-07-13 01:02:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,10 +7,8 @@ class TicTacToe
   WIN_COMBINATIONS = [0,1,2,], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [6,4,2], [0,4,8]
 
 
-
-
   def display_board
-    puts @board
+    puts @board[0] @board[1]
   end
 
   def move(i,token = "X")
