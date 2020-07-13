55853c6c01ec325eb0feba4cc4aecbeaf7eab4db
Jessie Donovan <donovanajessie@gmail.com>
2020-07-13 00:55:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,9 +4,10 @@ class TicTacToe
     @board = [" " ," " ," " ," " ," " ," " ," " ," " ," " ]
   end
 
-  def WIN_COMBINATIONS
+  WIN_COMBINATIONS = [1,2,3], [4,5,6], [7,8,9], [0,1,2], [1,4,7], [2,5,8], [6,4,2], [0,4,8] 
 
-  end
+  
+
 
   def display_board
     puts @board
