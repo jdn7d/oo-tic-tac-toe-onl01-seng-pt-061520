236607bdf567cd1a0a9b248effa174bbc73df2d9
Jessie Donovan <donovanajessie@gmail.com>
2020-07-13 01:30:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,8 +8,10 @@ class TicTacToe
 
 
   def display_board
-    puts "#{@board[0]} | #{@board[1]} | #{@board[2]} /n" ,
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]} /n" , 
+    "-------------------------"
     "#{@board[3]} | #{@board[4]} | #{@board[5]} /n",
+    "------------------------"
     "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
 
